ed0a9c99dc1a27f9f73a7f8da37cf983119e7c23
Interrupt <cuddigan@gmail.com>
2024-06-09 08:43:09 -0700

    Can compile our example for web now

--- main.zig
+++ main.zig
@@ -2,7 +2,20 @@ const std = @import("std");
 const delve = @import("delve");
 const app = delve.app;
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+
 pub fn main() !void {
+    // Pick the allocator to use depending on platform
+    const builtin = @import("builtin");
+    if (builtin.os.tag == .wasi or builtin.os.tag == .emscripten) {
+        // Web builds hack: use the C allocator to avoid OOM errors
+        // See https://github.com/ziglang/zig/issues/19072
+        try delve.init(std.heap.c_allocator);
+    } else {
+        try delve.init(gpa.allocator());
+    }
+
+    // create our module
     const example_module = delve.modules.Module{
         .name = "example",
         .draw_fn = on_draw,
@@ -17,5 +30,4 @@ pub fn main() !void {
 pub fn on_draw() void {
     delve.platform.graphics.setClearColor(delve.colors.Color.new(0.1, 0.1, 0.15, 1));
     delve.platform.graphics.drawDebugText(44, 40, "Hello Delve Framework!");
-    delve.api.draw.rectangle(40, 64, 450, 50, 6, 12);
 }
